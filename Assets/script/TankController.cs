7a1cf671fd40ebb7bfdafbde467ad17173de1a7c
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2022-04-29 22:12:20 +0530

    phase1

---
+++ Assets/script/TankController.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+public class TankController
+{
+    private TankModel tankModel;
+
+    private TankView tankView;
+}
---
+++ Assets/script/TankModel.cs
@@ -0,0 +1,7 @@
+
+using UnityEngine;
+
+public class TankModel
+{
+    private TankController tankController;
+}
